8c5286131e35dba9bd42fe88f970dc0c48f4a520
HMatthewsQA <HMatthews@QA.com>
2020-06-09 15:31:05 +0000

    commit for 09/06

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 import os
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = getenv('MY_SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from application import routes
