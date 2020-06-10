f579f4147aa07cbdd90074f0d6c17cfef955b4bf
HMatthewsQA <HMatthews@QA.com>
2020-06-10 14:58:25 +0000

    commit for 10/06

--- application/__init__.py
+++ application/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 import os
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -10,5 +11,7 @@ app.config['SECRET_KEY'] = getenv('MY_SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 from application import routes
--- application/forms.py
+++ application/forms.py
@@ -1,7 +1,49 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from application.models import Users
+from flask_login import current_user
+
+class UpdateAccountForm(FlaskForm):
+    first_name = StringField('First Name',
+        validators=[
+            DataRequired(),
+            Length(min=4, max=30)
+        ])
+    last_name = StringField('Last Name',
+        validators=[
+            DataRequired(),
+            Length(min=4, max=30)
+        ])
+    email = StringField('Email',
+        validators=[
+            DataRequired(),
+            Email()
+        ])
+    submit = SubmitField('Update')
+
+    def validate_email(self,email):
+        if email.data != current_user.email:
+            user = Users.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email already in use')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+        validators=[
+            DataRequired(),
+            Email()
+        ]
+    )
+
+    password = PasswordField('Password',
+        validators=[
+            DataRequired()
+        ]
+    )
+
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
 
 class RegistrationForm(FlaskForm):
     email = StringField('Email',
@@ -21,6 +63,16 @@ class RegistrationForm(FlaskForm):
             EqualTo('password')
         ]
     )
+    first_name = StringField('First Name',
+    validators=[
+        DataRequired(),
+        Length(min=2, max=30)
+    ])
+    last_name = StringField('Last Name',
+    validators=[
+        DataRequired(),
+        Length(min=3, max=30)
+    ])
     submit = SubmitField('Sign Up')
 
     def validate_email(self, email):
@@ -30,18 +82,6 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('Email already in use')
 
 class PostForm(FlaskForm):
-    first_name = StringField('First Name',
-        validators = [
-            DataRequired(),
-            Length(min=2, max=30)
-        ]
-    )
-    last_name = StringField('Last Name',
-        validators = [
-            DataRequired(),
-            Length(min=2, max=30)
-        ]
-    )
     title = StringField('Title',
         validators = [
             DataRequired(),
